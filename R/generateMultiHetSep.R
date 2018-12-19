f74a2d704c85f7e4c84cf257c9838d07cd932d08
Jesse Garci <jessegarcia@addr177.net185.lifesci.ucla.edu>
2018-12-19 13:37:25 -0800

    Ability to generateMultiHetSep for psmc from msprime function. Doesn't include ending position of genome though.

---
+++ R/generateMultiHetSep.R
@@ -0,0 +1,28 @@
+generateMultiHetSep<-function(msprimeVCF, outpath){
+  
+
+df<-dplyr::as_tibble(data.table::fread(msprimeVCF))
+
+df<-df %>%
+  dplyr::mutate(Homozygous=str_detect(msp_0, "1\\|1")) %>%
+  dplyr::mutate(Heterozygous=!Homozygous)
+
+heterozygous<-df %>%
+  dplyr::filter(Heterozygous==TRUE)
+
+#Distances between Heterozygous positions. Won't get the end of the chromosome. 
+heterozygousDifferences<-diff(c(1,heterozygous$POS))
+
+heterozygous$heterozygousDifferences<-heterozygousDifferences
+
+
+heterozygous<-heterozygous %>% 
+  dplyr::mutate(orderOfAlleles=stringr::str_replace(msp_0, pattern="\\|", replacement = ""))
+
+multihet<-heterozygous %>% 
+  dplyr::select(`#CHROM`, POS,heterozygousDifferences, orderOfAlleles ) %>%
+  dplyr::mutate(heterozygousDifferences=parse_integer(heterozygousDifferences))
+
+readr::write_tsv(x=multihet, path=outpath, col_names = FALSE)
+
+}
