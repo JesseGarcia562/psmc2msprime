4e7e4a90700c6c7f9a7696ee3a7801bdf4038602
Jesse Garci <jessegarcia@addr177.net185.lifesci.ucla.edu>
2018-12-19 13:31:05 -0800

    Added script to writeVCFOFSimulation.

---
+++ R/writeVCFOfSimulation.R
@@ -0,0 +1,8 @@
+writeVCFOfSimulation<-function(simulation, outpath, ploidy=2){
+  py <- reticulate::import_builtins()
+  with(py$open(outpath, "w") %as% file, {
+    simulation$write_vcf(file,  ploidy=parse_integer(ploidy))
+  })
+  
+  
+}
