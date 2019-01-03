4f3bb94fb913e7b692b110413c5f75d47d8858f4
Jesse Garci <jessegarcia@addr128.net185.lifesci.ucla.edu>
2019-01-03 10:34:47 -0800

    msmc_model needs to take in "totalGenomeLength"

--- R/withScriptGeneratePi.R
+++ R/withScriptGeneratePi.R
@@ -1,6 +1,6 @@
 withScriptGeneratePi<-function(scriptPath, outPath, totalGenomeLength=3000000, windowSize=500000){
   source_python(scriptPath)
-  simulation<-msmc_model(length=length, seed=30, mu=1.01e-08)
+  simulation<-msmc_model(length= totalGenomeLength, seed=30, mu=1.01e-08)
   
   writeVCFOfSimulation(simulation, outpath = outPath, ploidy=2)
   
